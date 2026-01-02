4a49ee991e6281e44b2430fe64f05e312c2e9692
Sithuli Basnayake <sithulibasnayake22@gmail.com>
2026-01-02 23:06:08 +0530

    added header
    added header with home ,properties,about ,contacts and favourites

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          Property<span>Hub</span>
+        </div>
+
+        <nav className="nav">
+          <a href="#home">Home</a>
+          <a href="#properties">Properties</a>
+          <a href="#favourites">Favourites</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+        </nav>
+
+        <button className="cta-btn">List Property</button>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
